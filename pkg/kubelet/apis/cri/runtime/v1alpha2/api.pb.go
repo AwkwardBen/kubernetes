2af8e0c511c5add83a2b5b625a630deb0d748bb3
Ben Eustace <awks.ben@gmail.com>
2018-07-03 23:12:57 +0100

    test

--- pkg/kubelet/apis/cri/runtime/v1alpha2/api.pb.go
+++ pkg/kubelet/apis/cri/runtime/v1alpha2/api.pb.go
@@ -3837,8 +3837,10 @@ func (c *runtimeServiceClient) Version(ctx context.Context, in *VersionRequest, 
 
 func (c *runtimeServiceClient) RunPodSandbox(ctx context.Context, in *RunPodSandboxRequest, opts ...grpc.CallOption) (*RunPodSandboxResponse, error) {
 	out := new(RunPodSandboxResponse)
-	fmt.Print("output test")
+	fmt.Print("output test\n")
 	fmt.Printf("%+v\n",opts)
+	fmt.Printf("%+v\n",in)
+	fmt.Printf("%+v\n",ctx)
 	err := grpc.Invoke(ctx, "/runtime.v1alpha2.RuntimeService/RunPodSandbox", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
